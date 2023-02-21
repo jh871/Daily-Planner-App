9120b564623dfbfa0ac20b6e98438f6b2a805419
jh871 <jennyh871@gmail.com>
2023-02-21 19:56:26 +0000

    added necessary q-selectors

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let currentDay = $("#currentDay");
+
+
+let hour = $(".hour");
+
+let timeBlock = $(".time-block");
+let textArea = $(".textarea");
+let block9 = $("#9am");
+let block10 = $("#10am");
+let block11= $("#11am");
+let block12 = $("#12pm");
+let block1 = $("#1pm");
+let block2 = $("#2pm");
+let block3 = $("#3pm");
+let block4 = $("#4pm");
+let block5 = $("#5pm");
+
+let saveButton = $(".saveBtn");
+
+
