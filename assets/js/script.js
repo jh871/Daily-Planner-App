ccd2bd09e16a31c8d31186174e833bad41d877e7
jh871 <jennyh871@gmail.com>
2023-02-22 22:38:42 +0000

    added logic to vary block colour by tense

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,17 @@ let hour = $(".hour");
 
 let timeBlock = $(".time-block");
 let textArea = $(".textarea");
-let block9 = $("#9am");
-let block10 = $("#10am");
-let block11= $("#11am");
-let block12 = $("#12pm");
-let block1 = $("#1pm");
-let block2 = $("#2pm");
-let block3 = $("#3pm");
-let block4 = $("#4pm");
-let block5 = $("#5pm");
+let block9 = $("#9");
+let block10 = $("#10");
+let block11= $("#11");
+let block12 = $("#12");
+let block1 = $("#13");
+let block2 = $("#14");
+let block3 = $("#15");
+let block4 = $("#16");
+let block5 = $("#17");
 
+let blocks = [block9, block10, block11, block12]
 let saveButton = $(".saveBtn");
 
 //display time
@@ -28,4 +29,21 @@ currentDay.html(date);
 
 //when user types in text area and clicks save, time is used as ls key and then text is stored in string;
 // color of block will changed based on whether it is future, present or past
+/*
+let time = parseInt(moment().format("H"));
+console.log(typeof time);
+*/
+let time = 13;
+
+
+let count = [block9, block10, block11, block12, block1, block2, block3, block4, block5];
+count.forEach((num) => {
+    if (parseInt(num[0].id) === time) {
+        num.addClass("present");
+    } else if (parseInt(num[0].id) < time){
+        num.addClass("past");
+    } else if (parseInt(num[0].id) > time) {
+        num.addClass("future");
+    }
+});
 
