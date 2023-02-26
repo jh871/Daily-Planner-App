4a6f87edac0e3d1ceaf3a7b1a8de8e50ba8fa5f7
jh871 <jennyh871@gmail.com>
2023-02-26 13:24:17 +0000

    local storage functions save data to ls

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ let hour = $(".hour");
 // ids for timeblocks
 let timeBlock = $(".time-block");
 let textArea = $(".textarea");
+
 let block9 = $("#9");
 let block10 = $("#10");
 let block11= $("#11");
@@ -19,8 +20,17 @@ let block3 = $("#15");
 let block4 = $("#16");
 let block5 = $("#17");
 
-let blocks = [block9, block10, block11, block12]
 let saveButton = $(".saveBtn");
+let btn9 = $("#btn9");
+let btn10 = $("#btn10");
+let btn11 = $("#btn11");
+let btn12 = $("#btn12");
+let btn13 = $("#btn13");
+let btn14 = $("#btn14");
+let btn15 = $("#btn15");
+let btn16 = $("#btn16");
+let btn17 = $("#btn17");
+
 
 //display time
 let date = moment().format("dddd, MMMM Do");
@@ -30,24 +40,19 @@ currentDay.html(date);
 //when user types in text area and clicks save, time is used as ls key and then text is stored in string;
 
 
-//change colour of timeblock by class:
-/*
-let time = parseInt(moment().format("H"));
-console.log(typeof time);
-*/
-let time = 13; //take out to set correctly
 
-let count = [block9, block10, block11, block12, block1, block2, block3, block4, block5];
-count.forEach((num) => {
-    if (parseInt(num[0].id) === time) {
-        num.addClass("present");
-    } else if (parseInt(num[0].id) < time){
-        num.addClass("past");
-    } else if (parseInt(num[0].id) > time) {
-        num.addClass("future");
-    }
-});
+// console.log(typeof time);
+// let time = 13; //take out to set correctly
 
+// console.log(block9[0]);
+// console.log(block10[0].id);
+// console.log(block10[0].value);
+
+
+// This worked but now decided to stop:
+
+
+// console.log(block9[0].id);
 
 
 // trying to save individual text to local storage
@@ -58,18 +63,23 @@ count.forEach((num) => {
 
 //     let storageTest = localStorage.getItem(saveButton[i].id);
 //     if (storageTest !== null){
-//         savedItem = JSON.parse(localStorage.getItem(saveButton[i].id));
+//         savedItem = localStorage.getItem(saveButton[i].id);
 //     };
-//     savedItem.push(blockText);
 // }
 
 
+// saveButton.on("click", function(event){
+//     let key = event.target.id;
+//     console.log(key);
 
-// console.log(typeof saveButton[8].id);
+// var txt = block10.val();
+// console.log(block10[0].value);
+
+// });
 
 
-// saveButton[i].on("click", saveText());
 
+//if block number.id = 
 
 
 // function saveText() {
@@ -80,7 +90,122 @@ count.forEach((num) => {
 // count[i].text("Hello " + count[i][0].id + "!")
 // };
 
-console.log(parseInt(count[0][0].id));
+// console.log(parseInt(count[0][0].id));
+// console.log(textArea[1].value)
+
+// Setting the local storage for each save button (in as many lines of code as possible apparently.)
+btn9.on("click", () => {
+    if (block9[0].value !== ""){
+    let lsTest = localStorage.getItem("9");
+    if (lsTest !== null) {
+        item9 = localStorage.getItem("9")
+        };
+    item9 = block9[0].value;
+    localStorage.setItem("9", item9);
+    };
+});
+
+btn10.on("click", () => {
+    if (block10[0].value !== ""){
+        let storageTest = localStorage.getItem("10");
+        if (storageTest !== null) {
+            item10 = localStorage.getItem("10")
+        };
+        item10 = block10[0].value;
+        localStorage.setItem("10", item10);
+    };
+});
+
+btn11.on("click", () => {
+    if (block11[0].value !== ""){
+        let lsTest = localStorage.getItem("11");
+        if (lsTest !== null) {
+            item11 = localStorage.getItem("11")
+            };
+        item11 = block11[0].value;
+        localStorage.setItem("11", item11);
+    };
+});
+
+btn12.on("click", () => {
+    if (block12[0].value !== ""){
+        let lsTest = localStorage.getItem("12");
+        if (lsTest !== null) {
+            item12 = localStorage.getItem("12")
+            };
+        item12 = block12[0].value;
+        localStorage.setItem("12", item12);
+    };
+});
+
+btn13.on("click", () => {
+    if (block1[0].value !== ""){
+        let lsTest = localStorage.getItem("13");
+        if (lsTest !== null) {
+            item13 = localStorage.getItem("13")
+            };
+        item13 = block1[0].value;
+        localStorage.setItem("13", item13);
+    };
+});
+
+btn14.on("click", () => {
+    if (block2[0].value !== ""){
+        let lsTest = localStorage.getItem("14");
+        if (lsTest !== null) {
+            item14 = localStorage.getItem("14")
+            };
+        item14 = block2[0].value;
+        localStorage.setItem("14", item14);
+    };
+});
+
+btn15.on("click", () => {
+    if (block3[0].value !== ""){
+        let lsTest = localStorage.getItem("15");
+        if (lsTest !== null) {
+            item15 = localStorage.getItem("15")
+            };
+        item15 = block3[0].value;
+        localStorage.setItem("15", item15);
+    };
+});
 
 
-console.log(textArea[1].value)
+btn16.on("click", () => {
+    if (block4[0].value !== ""){
+        let lsTest = localStorage.getItem("16");
+        if (lsTest !== null) {
+            item16 = localStorage.getItem("16")
+            };
+        item16 = block4[0].value;
+        localStorage.setItem("16", item16);
+    };
+});
+
+btn17.on("click", () => {
+    if (block5[0].value !== ""){
+        let lsTest = localStorage.getItem("17");
+        if (lsTest !== null) {
+            item17 = localStorage.getItem("17")
+            };
+        item17 = block5[0].value;
+        localStorage.setItem("17", item17);
+    };
+});;
+
+
+//change colour of timeblock by class:
+let time = parseInt(moment().format("H"));
+//this runs completely fine but also errors in the console??
+let countArray = [block9, block10, block11, block12, block1, block2, block3, block4, block5];
+
+countArray.forEach((element) => {
+    if (parseInt(element[0].id) === time) {
+        element.addClass("present");
+    } else if (parseInt(element[0].id) < time){
+        element.addClass("past");
+    } else if (parseInt(element[0].id) > time) {
+        element.addClass("future");
+    };
+});
