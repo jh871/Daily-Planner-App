a1731e8f36e371442b7bed65999b8d9dd2581e1a
jh871 <jennyh871@gmail.com>
2023-02-26 16:26:30 +0000

    user can overwrite cleared appt by saving empty textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,103 +50,86 @@ block5[0].value = localStorage.getItem("17");
 
 // Setting the local storage for each save button (in as many lines of code as possible apparently.)
 btn9.on("click", () => {
-    if (block9[0].value !== ""){
+    // if (block9[0].value !== ""){
     let lsTest = localStorage.getItem("9");
     if (lsTest !== null) {
         item9 = localStorage.getItem("9");
         };
     item9 = block9[0].value;
     localStorage.setItem("9", item9);
-    };
 });
 
 btn10.on("click", () => {
-    if (block10[0].value !== ""){
         let storageTest = localStorage.getItem("10");
         if (storageTest !== null) {
             item10 = localStorage.getItem("10");
         };
         item10 = block10[0].value;
         localStorage.setItem("10", item10);
-    };
 });
 
 btn11.on("click", () => {
-    if (block11[0].value !== ""){
         let lsTest = localStorage.getItem("11");
         if (lsTest !== null) {
             item11 = localStorage.getItem("11");
             };
         item11 = block11[0].value;
         localStorage.setItem("11", item11);
-    };
 });
 
 btn12.on("click", () => {
-    if (block12[0].value !== ""){
         let lsTest = localStorage.getItem("12");
         if (lsTest !== null) {
             item12 = localStorage.getItem("12");
             };
         item12 = block12[0].value;
         localStorage.setItem("12", item12);
-    };
 });
 
 btn13.on("click", () => {
-    if (block1[0].value !== ""){
         let lsTest = localStorage.getItem("13");
         if (lsTest !== null) {
             item13 = localStorage.getItem("13");
             };
         item13 = block1[0].value;
         localStorage.setItem("13", item13);
-    };
 });
 
 btn14.on("click", () => {
-    if (block2[0].value !== ""){
         let lsTest = localStorage.getItem("14");
         if (lsTest !== null) {
             item14 = localStorage.getItem("14");
             };
         item14 = block2[0].value;
         localStorage.setItem("14", item14);
-    };
 });
 
 btn15.on("click", () => {
-    if (block3[0].value !== ""){
         let lsTest = localStorage.getItem("15");
         if (lsTest !== null) {
             item15 = localStorage.getItem("15");
             };
         item15 = block3[0].value;
         localStorage.setItem("15", item15);
-    };
 });
 
 
 btn16.on("click", () => {
-    if (block4[0].value !== ""){
         let lsTest = localStorage.getItem("16");
         if (lsTest !== null) {
             item16 = localStorage.getItem("16");
             };
         item16 = block4[0].value;
         localStorage.setItem("16", item16);
-    };
 });
 
 btn17.on("click", () => {
-    if (block5[0].value !== ""){
         let lsTest = localStorage.getItem("17");
         if (lsTest !== null) {
             item17 = localStorage.getItem("17");
             };
         item17 = block5[0].value;
         localStorage.setItem("17", item17);
-    };
 });;
 
 
